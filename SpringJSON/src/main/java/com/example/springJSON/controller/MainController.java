93d57dc25d7a7576119c670ab5fb5a2a4eae2127
Pulkit Vaish <69940487+PulkitVaish@users.noreply.github.com>
2021-09-04 02:48:27 +0530

    Update MainController.java

--- SpringJSON/src/main/java/com/example/springJSON/controller/MainController.java
+++ SpringJSON/src/main/java/com/example/springJSON/controller/MainController.java
@@ -1,9 +1,7 @@
 package com.example.springJSON.controller;
 
-import org.springframework.stereotype.Controller;
 import java.util.concurrent.ThreadLocalRandom;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -40,66 +38,67 @@ public class MainController {
 	   @RequestMapping(value = "/comments")
 	   public JSONArray getComments(HttpServletRequest req) {
 		   try {
-	      HttpHeaders headers = new HttpHeaders();
-	      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-	      HttpEntity <String> entity = new HttpEntity<String>(headers);
-	      
-	      String total = req.getParameter("num");
-	      if(total==null) total = "1";//Param not set
-	      
-	      /** Getting data from randomuserapi **/
-	      String userString =  restTemplate.exchange("https://randomuser.me/api/?results="+total+"&inc=name,login,picture,registered&noinfo", HttpMethod.GET, entity, String.class).getBody();
-	      Object userObject=JSONValue.parse(userString);  
-	      JSONObject userData = (JSONObject) userObject;
-	      JSONArray results = (JSONArray) userData.get("results");
-	      
-	      JSONArray data = new JSONArray();
-	      
-	      for(int i=0;i<results.size();i++) {
-	    	  /** Getting data from asdfast **/
-	    	  int randomCommentLength = ThreadLocalRandom.current().nextInt(20, 40 + 1);
-	    	  int randomLikeCount = ThreadLocalRandom.current().nextInt(0, 999 + 1);
-		      String commentString =  restTemplate.exchange("https://asdfast.beobit.net/api/?length="+ Integer.toString(randomCommentLength)+"&type=word", HttpMethod.GET, entity, String.class).getBody();
-		      Object commentObject=JSONValue.parse(commentString);  
-		      JSONObject commentData = (JSONObject) commentObject;
-		      String comment = commentData.get("text").toString();
+		      HttpHeaders headers = new HttpHeaders();
+		      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		      HttpEntity <String> entity = new HttpEntity<String>(headers);
 		      
-		      /** Transforming into the required format of data **/
-		      JSONObject response = new JSONObject();
-		      JSONObject props = (JSONObject)results.get(i); 
+		      String total = req.getParameter("num");
+		      if(total==null) total = "1";//Param not set
 		      
-		      JSONObject nameObj = (JSONObject)props.get("name");
-		      String name = nameObj.get("first").toString()+" "+nameObj.get("last").toString();
-		      response.put("name", name);
+		      /** Getting data from randomuserapi **/
+		      String userString =  restTemplate.exchange("https://randomuser.me/api/?results="+total+"&inc=name,login,picture,registered&noinfo", HttpMethod.GET, entity, String.class).getBody();
+		      Object userObject=JSONValue.parse(userString);  
+		      JSONObject userData = (JSONObject) userObject;
+		      JSONArray results = (JSONArray) userData.get("results");
 		      
-		      JSONObject imgObj = (JSONObject)props.get("picture");
-		      String imgURL = imgObj.get("thumbnail").toString();
-		      response.put("avatar", imgURL);
+		      JSONArray data = new JSONArray();
 		      
-		      JSONObject loginObj = (JSONObject)props.get("login");
-		      String uid = loginObj.get("uuid").toString();
-		      String cid = loginObj.get("md5").toString();
-		      response.put("uid", uid);
-		      response.put("cid", cid);
+		      for(int i=0;i<results.size();i++) {
+		    	  /** Getting data from asdfast **/
+		    	  int randomCommentLength = ThreadLocalRandom.current().nextInt(20, 40 + 1);
+		    	  int randomLikeCount = ThreadLocalRandom.current().nextInt(0, 999 + 1);
+			      String commentString =  restTemplate.exchange("https://asdfast.beobit.net/api/?length="+ Integer.toString(randomCommentLength)+"&type=word", HttpMethod.GET, entity, String.class).getBody();
+			      Object commentObject=JSONValue.parse(commentString);  
+			      JSONObject commentData = (JSONObject) commentObject;
+			      String comment = commentData.get("text").toString();
+			      
+			      /** Transforming into the required format of data **/
+			      JSONObject response = new JSONObject();
+			      JSONObject props = (JSONObject)results.get(i); 
+			      
+			      JSONObject nameObj = (JSONObject)props.get("name");
+			      String name = nameObj.get("first").toString()+" "+nameObj.get("last").toString();
+			      response.put("name", name);
+			      
+			      JSONObject imgObj = (JSONObject)props.get("picture");
+			      String imgURL = imgObj.get("thumbnail").toString();
+			      response.put("avatar", imgURL);
+			      
+			      JSONObject loginObj = (JSONObject)props.get("login");
+			      String uid = loginObj.get("uuid").toString();
+			      String cid = loginObj.get("md5").toString();
+			      response.put("uid", uid);
+			      response.put("cid", cid);
+			      
+			      JSONObject dateObj = (JSONObject)props.get("registered");
+			      String isoDate = dateObj.get("date").toString();
+			      response.put("date", isoDate);
+			      
+			      response.put("comment",comment);
+			      response.put("likes",randomLikeCount);
+			      
+			      data.add(response);
+		      	}
 		      
-		      JSONObject dateObj = (JSONObject)props.get("registered");
-		      String isoDate = dateObj.get("date").toString();
-		      response.put("date", isoDate);
-		      
-		      response.put("comment",comment);
-		      response.put("likes",randomLikeCount);
-		      
-		      data.add(response);
-	      	}
 	      		return  data;
       
-	   	}catch (Exception e) {
-			JSONArray errorList = new JSONArray();
-			JSONObject error = new JSONObject();
-			error.put("Error", e.getMessage());
-			errorList.add(error);
-			return errorList;
-		}
+		   	}catch (Exception e) {
+				JSONArray errorList = new JSONArray();
+				JSONObject error = new JSONObject();
+				error.put("Error", e.getMessage());
+				errorList.add(error);
+				return errorList;
+			}
 	  }
 	   
 	   /*
@@ -111,7 +110,7 @@ public class MainController {
 	   @RequestMapping(value = "/reviews")
 	   public JSONArray getReviews(HttpServletRequest req) {
 		   try {
-			   HttpHeaders headers = new HttpHeaders();
+			   	  HttpHeaders headers = new HttpHeaders();
 			      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 			      HttpEntity <String> entity = new HttpEntity<String>(headers);
 			      
@@ -177,7 +176,7 @@ public class MainController {
 			error.put("Error", e.getMessage());
 			errorList.add(error);
 			return errorList;
-		}
+		   }
 	      
 	  }
 	   
